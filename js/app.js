2517e47b3a66bd758d75b9ccee3b1a7ff11d9f81
Tushar Kashyap <kashyap.tushar15@gmail.com>
2020-06-07 17:58:57 +0530

    Final build

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 function validateEmail() {
 	var err = document.getElementById("error").style.display="block";
+	document.getElementById("errMessage").textContent = "Please provide a valid Email";
 }
